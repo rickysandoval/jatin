ebafa5d660a3ade3a72e8544750bdfd3e14d946e
Ricky Sandoval <rsandoval@arrow.com>
2020-11-11 20:42:26 -0700

    podcast page

--- src/podcast/index.php
+++ src/podcast/index.php
@@ -7,14 +7,21 @@
     <link rel="stylesheet" href="../styles/main.css">
 </head>
 <body>
-    <?php include '../partials/header.php' ?>
-    <h1 class="page__title">Podcast</h1>
-    <div class="content-wrapper">
-       <main class="home__main">
-            <p class="text-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus consequuntur, nostrum optio iure quam recusandae ullam qui sit illo quaerat, nemo, sed distinctio ut vel nesciunt eaque harum. Cumque, tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at odit delectus, illo ab in reiciendis numquam aliquam consequatur reprehenderit minus ducimus ipsa quia ad. Blanditiis adipisci minus omnis culpa. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam adipisci facilis ducimus magni culpa a quis. Dolorum omnis nostrum sunt at possimus veritatis iure, saepe in! Amet veniam repellendus dolorum!</p>
-            <p class="text-content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non a, veritatis, doloribus earum quisquam omnis voluptate est, sed nobis saepe repellendus iure similique dolorum possimus laudantium ut officia officiis vel?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, laborum nihil dolor praesentium eveniet soluta molestias alias provident nostrum enim ipsum dolorem. Veniam aspernatur sequi nisi animi reiciendis doloremque similique?</p>
+    <div class="site-wrapper">
+        <?php include '../partials/header.php' ?>
+        <main>
+            <h1 class="page__title">Podcast</h1>
+            <section class="page__section">
+                <p class="text-content">Jatin is a young comedian, writer, environmentalist, and philosopher trying to make sense of it all.</p>
+                <p class="text-content">Join him as he explores the world and tries to understand the big questions: What is our purpose? What makes us truly happy? Should Kanye stop making music? At what age should your mom stop folding your laundry?</p>
+                <p class="text-content">With this podcast, he hopes to explore these topics and more, by interviewing other millennials who are trying to make the most of their time here on Earth.</p>
+            </section>
+            <ul class="podcast-list">
+                <li><iframe src="https://anchor.fm/justkiddingpod/embed/episodes/Share-Your-Story-with-Dylan-Owen-em1pqc" height="auto" width="100%" frameborder="0" scrolling="no"></iframe></li>
+                <li><iframe src="https://anchor.fm/justkiddingpod/embed/episodes/Election-Special-with-Shane-Skelton-eluegl/a-a3oop3u" height="auto" width="100%" frameborder="0" scrolling="no"></iframe></li>
+            </ul>
         </main>
+        <?php include '../partials/footer.php' ?>
     </div>
-    <?php include '../partials/footer.php' ?>
 </body>
 </html>
